d1ef61ead0360272c5bfe4e487207cd51f16b4c1
TahirAtakanCan <atkan4202@gmail.com>
2023-10-26 18:44:03 +0300

    Initial Commit

---
+++ ArtBookProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtBookProject
+//
+//  Created by Tahir Atakan Can on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
