92fb494ecef54858777349935ad7a82db3a50278
TahirAtakanCan <atkan4202@gmail.com>
2023-10-27 17:17:53 +0300

    feat : Image Update / Provides 3 different options(gallery,camera and exit) when clicked on the image

--- ArtBookProject/DetailsVC.swift
+++ ArtBookProject/DetailsVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController , UIImagePickerControllerDelegate , UINavigationControllerDelegate {
     
     
     @IBOutlet weak var imageView: UIImageView!
@@ -22,11 +22,79 @@ class DetailsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let gestureRecognizer = UITapGestureRecognizer
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //Recognizer      klavye kapatma
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeybord))
+        view.addGestureRecognizer(gestureRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecognizer)
+        
+        
+        
     }
     
     
-    @IBAction func saveButtonText(_ sender: Any) {
+    @IBAction func saveButtonText(_ sender: Any){
+        
+    }
+    
+    
+    @objc func hideKeybord(){
+        view.endEditing(true)
+    }
+    
+    @objc func selectImage(){
+        let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+
+            let alertController = UIAlertController(title: "Select Photo", message: "Where do you want to choose the picture?", preferredStyle: .actionSheet)
+
+            let galleryAction = UIAlertAction(title: "Photos", style: .default) { (action) in
+                if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                    imagePicker.sourceType = .photoLibrary
+                    self.present(imagePicker, animated: true, completion: nil)
+                } else {
+                    // Galeri kullanılamaz uyarısı
+                    // Örneğin: self.showAlert(message: "Galeri kullanılamıyor.")
+                }
+            }
+
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    imagePicker.sourceType = .camera
+                    self.present(imagePicker, animated: true, completion: nil)
+                } else {
+                    // Kamera kullanılamaz uyarısı
+                    // Örneğin: self.showAlert(message: "Kamera kullanılamıyor.")
+                }
+            }
+
+            let cancelAction = UIAlertAction(title: "Exit", style: .cancel, handler: nil)
+
+            alertController.addAction(galleryAction)
+            alertController.addAction(cameraAction)
+            alertController.addAction(cancelAction)
+
+            present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    // medya ile işimiz bittiğinde bir şeyler döndürmek için
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage  //opsiyonel olarak
+        self.dismiss(animated: true, completion: nil)
     }
     
     
