26161c1ba6f414dd3356da3720925ac82da1a38e
TahirAtakanCan <atkan4202@gmail.com>
2023-10-31 12:36:38 +0300

    finished project

--- ArtBookProject/DetailsVC.swift
+++ ArtBookProject/DetailsVC.swift
@@ -19,9 +19,68 @@ class DetailsVC: UIViewController , UIImagePickerControllerDelegate , UINavigati
     
     @IBOutlet weak var yearText: UITextField!
     
+    @IBOutlet weak var saveButton: UIButton!
+    
+    
+    var chosenPainting = ""
+    var chosenPaintingId : UUID?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        if chosenPainting != "" {
+            
+            saveButton.isHidden = true
+            
+            //Core Data
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            
+            let idString = chosenPaintingId?.uuidString
+            
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString!)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do{
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        if let name = result.value(forKey: "name") as? String {
+                            nameText.text = name
+                        }
+                        if let artist = result.value(forKey: "artist") as? String{
+                            artistText.text = artist
+                        }
+                        if let year = result.value(forKey: "year") as? Int{
+                            yearText.text = String(year)
+                        }
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                        }
+                    }
+                }
+            }
+            catch{
+                print("error")
+            }
+            
+            
+            
+            
+            
+        }else{
+            saveButton.isHidden = false
+            saveButton.isEnabled = false
+            nameText.text = ""
+            artistText.text = ""
+            yearText.text = ""
+        }
         
         
         
@@ -39,46 +98,8 @@ class DetailsVC: UIViewController , UIImagePickerControllerDelegate , UINavigati
     }
     
     
-    @IBAction func saveButtonText(_ sender: Any){
-        
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
-        
-        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
-        
-        // Attributes
-        
-        newPainting.setValue(nameText.text, forKey: "name")
-        newPainting.setValue(artistText.text, forKey: "artist")
-        
-        if let year = Int(yearText.text!){
-            newPainting.setValue(year, forKey: "year")
-        }
-        
-        newPainting.setValue(UUID(), forKey: "id")
-        
-        let data = imageView.image!.jpegData(compressionQuality: 0.5)
-        newPainting.setValue(data, forKey: "image")
-        
-        // hata çıkabilir sadece bu şekil yazamam
-        //context.save()
-        
-        do{
-            try context.save()
-            print("success")
-        }catch {
-            print("error")
-        }
-        
-        
-        
-        
-    }
     
     
-    @objc func hideKeybord(){
-        view.endEditing(true)
-    }
     
     @objc func selectImage(){
         let imagePicker = UIImagePickerController()
@@ -118,11 +139,56 @@ class DetailsVC: UIViewController , UIImagePickerControllerDelegate , UINavigati
         
     }
     
+    @objc func hideKeybord(){
+        view.endEditing(true)
+    }
+    
+    
     // medya ile işimiz bittiğinde bir şeyler döndürmek için
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage  //opsiyonel olarak
+        saveButton.isEnabled = true 
         self.dismiss(animated: true, completion: nil)
     }
     
     
+    @IBAction func saveButtonText(_ sender: Any){
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        // Attributes
+        
+        newPainting.setValue(nameText.text, forKey: "name")
+        newPainting.setValue(artistText.text, forKey: "artist")
+        
+        if let year = Int(yearText.text!){
+            newPainting.setValue(year, forKey: "year")
+        }
+        
+        newPainting.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image!.jpegData(compressionQuality: 0.5)
+        newPainting.setValue(data, forKey: "image")
+        
+        // hata çıkabilir sadece bu şekil yazamam
+        //context.save()
+        
+        do{
+            try context.save()
+            print("success")
+        }catch {
+            print("error")
+        }
+        
+        NotificationCenter.default.post(name: NSNotification.Name("newData"), object: nil)  //2. yol -> bu kaydettiğimiz veriyi gösteriyor
+        self.navigationController?.popViewController(animated: true)
+        
+        
+    }
+
+    
+
 }
